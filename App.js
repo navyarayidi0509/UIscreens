3c340adcf16ff7a25ba6a3c3c5d62d42a9d6d34b
navyarayidi0509 <121232891+navyarayidi0509@users.noreply.github.com>
2023-03-15 13:28:42 +0530

    adding navigation drawer example

--- App.js
+++ App.js
@@ -1,65 +1,53 @@
-import React from 'react';
-import { Image, ImageBackground, StyleSheet ,Text,View} from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import image from './assets/img1.png';
-import logo from './assets/logo.png';
-import P from './Screens/S1';
-import DetailScreen from './Screens/DetailScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Stack = createStackNavigator();
-
-const App = () => {
+function ProfileScreen({ navigation }) {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-        <Stack.Screen name="Details" component={DetailScreen} options={{headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const HomeScreen = () => {
-  return (
-    <ImageBackground source={image} style={styles.image}>
-      <Image source={logo} style={styles.logo}/>
-      <P />
-    </ImageBackground>
-  );
-};
-
-const DetailsScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Details Screen</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Profile Screen</Text>
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  image: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
-  logo:{
-    width: '50%', 
-    aspectRatio: 1, 
-    position: 'absolute', 
-    top: '20%', 
-    left: '7%', 
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
 
-export default App;
+function Articles({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>ArticlesScreen</Text>
+    </View>
+  );
+}
+
+function logout({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>logout page</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home" useLegacyImplementation>
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Profile" component={ProfileScreen} />
+        <Drawer.Screen name="Articles" component={Articles} />
+        <Drawer.Screen name="logout" component={logout} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
